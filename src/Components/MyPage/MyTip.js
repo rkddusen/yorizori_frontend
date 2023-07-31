2df94ac1657aec555f7e70b4ab896a1f39e37b21
rkddusen <k99endus@naver.com>
2023-07-31 11:54:54 +0900

    edit Tip View with Tip pages

--- src/Components/MyPage/MyTip.js
+++ src/Components/MyPage/MyTip.js
@@ -1,30 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserContext } from '../../contexts/UserContext';
 import { styled } from 'styled-components';
 import TipView from '../TipView'
 
 const MyTip = () => {
   const { user } = useUserContext();
-  const [tip, setTip] = useState([
-    { id: 1, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 2, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 3, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 4, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 5, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 6, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 7, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 8, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 9, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 10, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 11, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 12, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-  ]);
-  
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    let _result = [];
+    for(let i = 0; i < 38; i++){
+      _result.push(
+        <TipView
+          key={i}
+          tip={
+            {
+              id: i+1,
+              profileImg: "https://yorizori-s3.s3.ap-northeast-2.amazonaws.com/userImage/sample.png",
+              nickname: "duyyaa",
+              thumbnail: "https://yorizori-s3.s3.ap-northeast-2.amazonaws.com/src/8455f69d-6f83-4a85-9f95-a577c8d807bf.jpg",
+              title: "제목",
+              heartCount: 100,
+              viewCount: 100,
+            }
+          }
+        />
+      )
+    }
+    setResult(_result);
+  },[]);
+
   return (
-    <>
-      <TipView tip={tip} />
-    </>
+    <TipList>
+      {result}
+    </TipList>
   );
 }
+
+const TipList = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  text-align: left;
+  flex-wrap: wrap;
+  column-gap: 2%;
+  @media screen and (max-width: 1023px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
 
 export default MyTip;
--- src/Pages/TipPage.js
+++ src/Pages/TipPage.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import styled from "styled-components";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
@@ -6,32 +7,46 @@ import TipView from "../Components/TipView"
 import Paging from '../Components/Paging'
 import PageExplain from '../Components/PageExplain';
 
-function TipPage(props) {
-  const { user } = props;
-  const [tip, setTip] = useState([
-    { id: 1, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 2, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 3, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 4, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 5, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 6, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 7, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 8, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 9, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 10, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 11, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-    { id: 12, img: './images/tip_thumbnail.jpg', title: '팁 제목', heartCount: '999+', opinionCount: '499+' },
-  ]);
-  const [totalTipCount, SetTotalTipCount] = useState(200);
+function TipPage() {
+  const [result, setResult] = useState([]);
+  const [totalTipCount, setTotalTipCount] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    let _result = [];
+    for(let i = 0; i < 12; i++){
+      _result.push(
+        <TipView
+          key={i}
+          tip={
+            {
+              id: i+1,
+              profileImg: "https://yorizori-s3.s3.ap-northeast-2.amazonaws.com/userImage/sample.png",
+              nickname: "duyyaa",
+              thumbnail: "https://yorizori-s3.s3.ap-northeast-2.amazonaws.com/src/8455f69d-6f83-4a85-9f95-a577c8d807bf.jpg",
+              title: "제목",
+              heartCount: 100,
+              viewCount: 100,
+            }
+          }
+        />
+      )
+    }
+    let _count = 200;
+    setResult(_result);
+    if(_count !== totalTipCount) setTotalTipCount(_count);
+  }, [location]);
 
   return (
     <div>
       <Wrap>
-      <Header user={user} />
+      <Header />
       <StyledBody>
         <Contents>
           <PageExplain title="COOKING TIP" explain="요리에 도움이 될 팁을 살펴보세요!"/>
-          <TipView tip={tip} />
+          <TipList>
+            {result}
+          </TipList>
           <Paging pagingCount={Math.ceil(totalTipCount/12)} />
         </Contents>
       </StyledBody>
@@ -57,6 +72,20 @@ const Contents = styled.div`
   @media screen and (max-width: 767px) {
     max-width: 400px;
     margin-top: 130px;
+  }
+`;
+const TipList = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  text-align: left;
+  flex-wrap: wrap;
+  column-gap: 2%;
+  @media screen and (max-width: 1023px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr);
   }
 `;
 
