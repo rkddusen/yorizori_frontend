468168f4152870bdd6b14f9efdd47a61fa336749
rkddusen <k99endus@naver.com>
2023-08-06 16:20:28 +0900

    ready to send review to server

--- src/Components/Evaluation.js
+++ src/Components/Evaluation.js
@@ -1,7 +1,7 @@
 // 평가와 관련된 Star, Opinion, Heart를 정의
 import React from "react";
 
-export const Star = ({ size = 16, color = "#FFA800" }) => (
+export const Star = ({ size = 16, color = "#FFA800", onClick }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -12,6 +12,7 @@ export const Star = ({ size = 16, color = "#FFA800" }) => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
+    onClick={onClick}
   >
     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
   </svg>
