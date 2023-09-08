e4eb1971400a917cfa0ed826689126262c5711cd
rkddusen <k99endus@naver.com>
2023-09-08 12:12:31 +0900

    get tip info

---
+++ src/Pages/TipReadPage.js
@@ -0,0 +1,373 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import { Heart } from "../Components/Evaluation";
+import { useUserContext } from '../contexts/UserContext';
+import { ProfileImg } from '../Components/ProfileImg';
+
+const TipReadPage = () => {
+  const { user } = useUserContext();
+  const [tip, setTip] = useState(null);
+  const [review, setReview] = useState(null);
+  const [isHeart, setIsHeart] = useState(false);
+  const params = useParams();
+  const textRef = useRef(null);
+  const axiosUrl = process.env.REACT_APP_AXIOS_URL;
+
+  const getTip = async () => {
+    const res = await axios.get(`${axiosUrl}/tip/get/details?tipId=${params.id}`);
+    try {
+      let _tip = {};
+      _tip.id = res.data.tipId;
+      _tip.thumbnail = res.data.tipThumbnail;
+      _tip.title = res.data.tipTitle;
+      _tip.profileImg = res.data.profileImg;
+      _tip.nickname = res.data.nickname;
+      _tip.date = res.data.date;
+      _tip.tipDetail = res.data.tipDetail;
+      _tip.viewCount = res.data.tipViewCount;
+      _tip.heartCount = res.data.tipHeartCount;
+      setTip(_tip);
+    } catch {
+      console.log("오류");
+    }
+  };
+  useEffect(() => {
+    getTip();
+    getReview();
+    getIsHeart();
+  }, [user]);
+
+  const saveReview = () => {
+    let content = {};
+    content['text'] = textRef.current.value;
+    content['userTokenId'] = user.id;
+    if(content['userTokenId']){
+      if(content['text'].length > 0){
+        axios.post(`${axiosUrl}/user/save/tip/review/${params.id}`, content)
+        .then(() => {
+          textRef.current.value = null;
+          getReview();
+        })
+        .catch(() => {
+          console.log('오류');
+        });
+      }
+      else {
+        window.alert('평가가 덜 됐음.')
+      }
+    } else {
+      window.alert('로그인 필요');
+    }
+  }
+  const getReview = async () => {
+    const res = await axios.get(`${axiosUrl}/tip/get/reviews/${params.id}`);
+    try {
+      console.log(res);
+      let _review = {};
+      _review.reviewCount = res.data.reviewCount;
+      _review.reviews = res.data.reviews;
+      setReview(_review);
+    } catch {
+      console.log("오류");
+    }
+  }
+  const handleHeartClick = () => {
+    isHeart ? saveIsHeart(false) : saveIsHeart(true);
+  }
+  const getIsHeart = async () => {
+    if(user.id){
+      const res = await axios.get(`${axiosUrl}/user/get/tip/isHeart/${params.id}?userId=${user.id}`);
+      console.log(res);
+    try {
+      let _isHeart = res.data.heart;
+      setIsHeart(_isHeart);
+    } catch {
+      console.log("오류");
+    }
+    }
+    
+  }
+  const saveIsHeart = async (bool) => {
+    const res = await axios.post(`${axiosUrl}/user/save/tip/isHeart/${params.id}?userId=${user.id}&isHeart=${bool}`);
+    try {
+      getIsHeart();
+    } catch {
+      console.log("오류");
+    }
+  }
+
+  return (
+    <div>
+      <Wrap>
+        <Header />
+        <StyledBody>
+          <Contents>
+            <RecipeContents>
+              <RecipeTitle>
+                <Thumbnail>
+                  <img src={process.env.REACT_APP_IMG_URL + tip?.thumbnail} />
+                </Thumbnail>
+                <Title>{tip?.title}</Title>
+                <HeartBox onClick={handleHeartClick}>
+                  <Heart size={22} color={isHeart ? '#FFA800' : '#dfdfdf'} />
+                  {tip?.heartCount}
+                </HeartBox>
+                <Explain>
+                  <Profile>
+                    <ProfileImg src={tip?.profileImg} style={{width: '35px', heigth: '35px', marginRight: '10px'}} />
+                    <ProfileNickname>{tip?.nickname}</ProfileNickname>
+                  </Profile>
+                  <ExplainSemi>
+                    <p>조회수 {tip?.viewCount}회</p>
+                    <p>{tip?.date}</p>
+                  </ExplainSemi>
+                </Explain>
+              </RecipeTitle>
+              <RecipeDetailBox>
+                <BoxTitle>
+                  <p>팁</p>
+                </BoxTitle>
+                {tip?.tipDetail}
+              </RecipeDetailBox>
+              <RecipeDetailBox>
+                <BoxTitle>
+                  <div>
+                    <p>댓글</p>
+                    <p>{review?.reviewCount}</p>
+                  </div>
+                </BoxTitle>
+                <ReviewWriteBox>
+                  <ReviewTextArea rows={3} placeholder='댓글을 작성해주세요.' ref={textRef} />
+                  <ReviewSubmitBtn onClick={saveReview}>작성</ReviewSubmitBtn>
+                </ReviewWriteBox>
+                <div>
+                  {review?.reviews.map((e, i) => (
+                    <ReviewList key={i}>
+                      <div>
+                        <img src={e.profileImg} />
+                      </div>
+                      <div>
+                        <ReviewListProfile>{e.nickname}</ReviewListProfile>
+                        <ReviewListContent>{e.review}</ReviewListContent>
+                        <ReviewListDate>{e.date}</ReviewListDate>
+                      </div>
+                    </ReviewList>
+                  ))}
+                </div>
+              </RecipeDetailBox>
+            </RecipeContents>
+          </Contents>
+        </StyledBody>
+      </Wrap>
+      <Footer />
+    </div>
+  );
+};
+
+const Wrap = styled.div`
+  width: 100%;
+  height: auto;
+  min-height: calc(100vh - 250px - 170px);
+`;
+const StyledBody = styled.div`
+  width: 100%;
+`;
+const Contents = styled.div`
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 150px;
+  @media screen and (max-width: 767px) {
+    max-width: 400px;
+    margin-top: 130px;
+  }
+`;
+
+const RecipeContents = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  @media screen and (max-width: 1023px) {
+    width: 70%;
+  }
+  @media screen and (max-width: 767px) {
+    width: 100%;
+  }
+`;
+const RecipeTitle = styled.div`
+  width: 100%;
+`;
+const Thumbnail = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  position: relative;
+  padding-bottom: 60%;
+
+  & > img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+`;
+const Title = styled.div`
+  width: 100%;
+  font-size: 22px;
+  text-align: start;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  @media screen and (max-width: 767px) {
+    font-size: 20px;
+  }
+`;
+const HeartBox = styled.button`
+  width: 100px;
+  height: 40px;
+  border: none;
+  background-color: #F5F5F5;
+  border-radius: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  & > svg{
+    margin-right: 10px;
+    flex-shrink: 0;
+  }
+  &:hover{
+    cursor: pointer;
+  }
+`;
+
+const Explain = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  background-color: #f5f5f5;
+  padding: 15px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+const Profile = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+`;
+
+const ProfileNickname = styled.p`
+  font-size: 18px;
+`;
+
+const ExplainSemi = styled.div`
+  margin-top: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  & > p:first-child {
+    margin-right: 10px;
+  }
+`;
+
+const RecipeDetailBox = styled.div`
+  width: 100%;
+  margin-top: 50px;
+`;
+const BoxTitle = styled.div`
+  font-size: 22px;
+  padding-bottom: 10px;
+  border-bottom: 2px solid #ffa800;
+  text-align: start;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+
+  & > div:first-child{
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+
+    & > p:first-child{
+      margin-right: 10px;
+    }
+    & > p:last-child{
+      color: #ffa800;
+    }
+  }
+`;
+
+
+const ReviewWriteBox = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #efefef;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const ReviewTextArea = styled.textarea`
+  width: 100%;
+  margin: 10px 0;
+  font-size: 16px;
+  border: none;
+  outline: none;
+  resize: none;
+`;
+const ReviewSubmitBtn = styled.button`
+  border: none;
+  border-radius: 5px;
+  background-color: #FFA800;
+  padding: 5px 10px;
+  font-size: 14px;
+  color: white;
+  &:hover{
+    cursor: pointer;
+  }
+`;
+const ReviewList = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  margin-top: 20px;
+  padding: 0 10px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid #efefef;
+  display: flex;
+  flex-direction: row;
+
+  & > div > img{
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    margin-right: 5px;
+  }
+`;
+const ReviewListStar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  margin-bottom: 5px;
+`;
+const ReviewListProfile = styled.p`
+  margin-bottom: 10px;
+  font-size: 10px;
+  color: #aaaaaa;
+`;
+const ReviewListContent = styled.div`
+  margin-bottom: 10px;
+  font-size: 14px;
+`;
+const ReviewListDate = styled.p`
+  font-size: 12px;
+  color: #aaaaaa;
+`;
+
+export default TipReadPage;
