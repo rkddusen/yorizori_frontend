29dec65abbe30665bc39d491f9337ec2b997cd6b
rkddusen <k99endus@naver.com>
2023-10-10 14:17:43 +0900

    프로필 동의 항목 삭제

--- src/Components/MyPage/ProfileChange.js
+++ src/Components/MyPage/ProfileChange.js
@@ -175,12 +175,6 @@ const ProfileChange = () => {
                   </SelectInput>
                 </Row>
               </Col>
-              <Col>
-                <AgreeRow>
-                  <p>개인 정보 수집 동의(?)</p>
-                  <StyledSvg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFA800" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></StyledSvg>
-                </AgreeRow>
-              </Col>
             </ProfileBox>
             <FinishArea>
               <button onClick={submitProfileChange}>적용</button>
